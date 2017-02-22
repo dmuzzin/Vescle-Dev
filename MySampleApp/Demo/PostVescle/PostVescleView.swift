daf7d4307480f16807dcf3c5fd727b4c9aa54e75
Jonathan Sussman <jesuss@umich.edu>
2017-02-22 12:38:36 -0500

    good status

--- MySampleApp/Demo/PostVescle/PostVescleView.swift
+++ MySampleApp/Demo/PostVescle/PostVescleView.swift
@@ -10,65 +10,46 @@ import Foundation
 import UIKit
 import AVFoundation
 
-class PostVescleViewController: UIViewController {
-    let captureSession = AVCaptureSession()
-    var previewLayer : AVCaptureVideoPreviewLayer?
+class PostVescleViewController: UIViewController,
+                                UIImagePickerControllerDelegate,
+                                UINavigationControllerDelegate {
+
     
-    // If we find a device we'll store it here for later use
-    var captureDevice : AVCaptureDevice?
+    
+    
+    @IBOutlet weak var ImagePicked: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+ 
         
-        // Do any additional setup after loading the view, typically from a nib.
-        captureSession.sessionPreset = AVCaptureSessionPresetHigh
-        
-        let devices = AVCaptureDevice.devices()
-        
-        // Loop through all the capture devices on this phone
-        for device in devices! {
-            // Make sure this particular device supports video
-            if ((device as AnyObject).hasMediaType(AVMediaTypeVideo)) {
-                // Finally check the position and confirm we've got the back camera
-                if((device as AnyObject).position == AVCaptureDevicePosition.back) {
-                    captureDevice = device as? AVCaptureDevice
-                    if captureDevice != nil {
-                        print("Capture device found")
-                        beginSession()
-                    }
-                }
-            }
-        }
         
     }
     
-    func configureDevice() {
-        if let device = captureDevice {
-            if device.lockForConfiguration() else {
-                throw kCMClockError_AllocationFailed;
-            }
-            device.focusMode = .locked
-            device.unlockForConfiguration()
+    @IBAction func openCameraButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
         }
-        
     }
     
-    func beginSession() {
-        
-        configureDevice()
-        
-        var err : NSError? = nil
-        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
-        
-        if err != nil {
-            println("error: \(err?.localizedDescription)")
+    @IBAction func openPhotoLibraryButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
         }
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        self.view.layer.addSublayer(previewLayer)
-        previewLayer?.frame = self.view.layer.frame
-        captureSession.startRunning()
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        ImagePicked.image = image
+        self.dismiss(animated: true, completion: nil);
     }
 }
     
