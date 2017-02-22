8f5611b07ebe9bc7754a784fbea716a9c3637056
Jonathan Sussman <jesuss@umich.edu>
2017-02-22 13:43:38 -0500

    asd

--- MySampleApp/Demo/PostVescle/PostVescleView.swift
+++ MySampleApp/Demo/PostVescle/PostVescleView.swift
@@ -10,26 +10,22 @@ import Foundation
 import UIKit
 import AVFoundation
 
-class PostVescleViewController: UIViewController,
-                                UIImagePickerControllerDelegate,
-                                UINavigationControllerDelegate {
-
+class PostVescleViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
-    
-    
+    @IBOutlet weak var TimePicker: UIPickerView!
     @IBOutlet weak var ImagePicked: UIImageView!
+    let imagePicker = UIImagePickerController()
+    let pickerDataSource = ["10 min", "30 min", "1 hr", "2 hrs", "5 hrs", "10 hrs" , "1 day", "2 days"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
- 
-        
-        
+        imagePicker.delegate = self
+       
     }
     
     @IBAction func openCameraButton(sender: AnyObject) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
-            let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
             imagePicker.allowsEditing = false
@@ -39,7 +35,6 @@ class PostVescleViewController: UIViewController,
     
     @IBAction func openPhotoLibraryButton(sender: AnyObject) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
-            let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
             imagePicker.allowsEditing = true
@@ -47,9 +42,20 @@ class PostVescleViewController: UIViewController,
         }
     }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
-        ImagePicked.image = image
-        self.dismiss(animated: true, completion: nil);
+      func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            ImagePicked.contentMode = .scaleAspectFit
+            ImagePicked.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
     }
-}
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
+
